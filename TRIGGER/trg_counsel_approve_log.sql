0fe8d7d779a62b8404dab28adada60afce1943e8
ggj0228 <ggj0228@naver.com>
2025-06-08 23:04:23 +0900

    trigger 주석 추가

--- TRIGGER/trg_counsel_approve_log.sql
+++ TRIGGER/trg_counsel_approve_log.sql
@@ -1,8 +1,10 @@
+
+-- 상담 게시글 상태가 '대기중'에서 '승인'으로 바뀌었을 때만 로그 기록
 DELIMITER //
 
 CREATE TRIGGER trg_counsel_approve_log
-AFTER UPDATE ON counsel
-FOR EACH ROW
+AFTER UPDATE ON counsel                   -- counsel 테이블의 UPDATE 이후 실행
+FOR EACH ROW                              -- 각 행마다 적용
 BEGIN
    
     IF OLD.status = '대기중' AND NEW.status = '승인' THEN           -- 상태가 '대기중'에서 '승인'으로 바뀌었을 때만 로그 기록
