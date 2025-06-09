edf06af749a2a9535ef55c7b6e1332860d334c22
jinnn12 <prohh12@naver.com>
2025-06-09 10:06:16 +0900

    admin update post

---
+++ DML/admin_update_post_status.sql
@@ -0,0 +1,45 @@
+DELIMITER //
+
+CREATE PROCEDURE admin_update_post_status (
+    IN p_post_id BIGINT,
+    IN p_admin_id BIGINT,
+    IN p_post_status ENUM('승인', '반려'),
+    IN p_rejected_reason VARCHAR(255)
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = '게시글 상태 변경 중 오류 발생';
+    END;
+
+    START TRANSACTION;
+
+    -- 상태가 승인일 경우 반려 사유는 NULL로 처리
+    IF p_post_status = '승인' THEN
+        UPDATE post
+        SET 
+            post_status = p_post_status,
+            post_rejected_reason = NULL,
+            approve_admin = p_admin_id,
+            post_updated_at = NOW()
+        WHERE post_id = p_post_id;
+    ELSEIF p_post_status = '반려' THEN
+        UPDATE post
+        SET 
+            post_status = p_post_status,
+            post_rejected_reason = p_rejected_reason,
+            approve_admin = p_admin_id,
+            post_updated_at = NOW()
+        WHERE post_id = p_post_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'post_status는 승인 또는 반려만 허용됩니다';
+    END IF;
+
+    COMMIT;
+END;
+//
+
+DELIMITER ;
